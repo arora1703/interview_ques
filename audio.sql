f6679cbe31bea9abb307f4b981d57c8a74a4dde6
arora1703 <abhimanyu.arora@adgonline.in>
2023-06-16 17:20:44 +0530

    first commit

---
+++ audio.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jan 05, 2023 at 06:09 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `interview_ques`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `audio`
+--
+
+CREATE TABLE `audio` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `audio` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `audio`
+--
+
+INSERT INTO `audio` (`id`, `audio`, `file`, `created_at`, `updated_at`) VALUES
+(1, 'typical-trap-loop-140bpm-129880.mp3', '20230513044357.mp3', '2023-05-13 23:16:05', '2023-05-13 23:16:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `audio`
+--
+ALTER TABLE `audio`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `audio`
+--
+ALTER TABLE `audio`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
